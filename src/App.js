2892b820ceb2953944e330e92dc9ea817ba77c44
Jakiepoo <joehersheyu45@gmail.com>
2022-05-13 22:00:52 -0700

    Added Screens (Calorie Intake, Home, Journal, Login, Nutrition, Recipes, Signup) and Navbar component.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Login from "./Screen/Signup/Login/Login";
+import Signup from "./Screen/Signup/Signup"
+import Home from "./Screen/Home/Home"
+import CalorieIntake from "./Screen/CalorieIntake/CalorieIntake"
+import Nutrition from "./Screen/Nutrition/Nutrition"
+import Recipes from "./Screen/Recipes/Recipes"
+import Journal from "./Screen/Journal/Journal"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Switch>
+      <Route exact path="/" component={Login}/>
+      <Route exact path="/signup" component={Signup}/>
+      <Route exact path="/home" component={Home}/>
+      <Route exact path="/calorie-intake" component={CalorieIntake}/>
+      <Route exact path="/nutrition" component={Nutrition}/>
+      <Route exact path="/recipes" component={Recipes}/>
+      <Route exact path="/journal" component={Journal}/>
+    </Switch>
+    </>
   );
 }
 
