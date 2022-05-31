8bdeea16d0c0c3c32b64fd906febc45256b2a8a9
Jakiepoo <joehersheyu45@gmail.com>
2022-05-30 22:06:44 -0700

    Fixed calorie-intake route type and deleted manifests for the time being

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { StyledNavbar, StyledLink, StyledUl, StyledLogo } from '../Styles/Navbar.styled'
 
-const Logo = (process.env.PUBLIC_URL + '/logo.png')
+export const Logo = (process.env.PUBLIC_URL + '/logo.png')
 
 const Navbar = () => {
     return (
@@ -9,7 +9,7 @@ const Navbar = () => {
             <StyledNavbar>
                 <StyledLogo src={Logo}/>
                 <StyledUl>
-                    <StyledLink to={"/calorieintake"}>
+                    <StyledLink to={"/calorie-intake"}>
                         Calorie Intake
                     </StyledLink>
                     <StyledLink to={"/nutrition"}>
